af9a4e8bfff9d70aad733f253537d931485a6eb3
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-04-01 11:39:38 +0100

    Fixed consent_tsi issue for national CSV export

--- configuration/queries/utilities/coronavirus-admin-national-results.sql
+++ configuration/queries/utilities/coronavirus-admin-national-results.sql
@@ -1,1 +1,19 @@
-SELECT first_name,last_name,email,constituency,nuts_3,simd_datazone,simd_rank,ward,tsi,consent_tsi,date_last_modified FROM newsletter_coronavirus WHERE consent_volunteering_updates = true AND cast(date_last_modified as date) between '{{#unless @root.request.params.query.from_date}}2020-03-29{{else}}{{@root.request.params.query.from_date}}{{/unless}}' and CURDATE() ORDER BY date_last_modified ASC;
+SELECT
+  first_name,
+  last_name,
+  email,
+  constituency,
+  nuts_3,
+  simd_datazone,
+  simd_rank,
+  ward,
+  tsi,
+  CASE WHEN consent_tsi = 1 THEN 'Yes' ELSE 'NO' END AS consent_tsi,
+  date_last_modified
+FROM newsletter_coronavirus
+WHERE
+  consent_volunteering_updates = true AND
+  CAST(date_last_modified AS date) between
+    '{{#unless @root.request.params.query.from_date}}2020-03-29{{else}}{{@root.request.params.query.from_date}}{{/unless}}' AND
+    CURDATE()
+ORDER BY date_last_modified ASC;
