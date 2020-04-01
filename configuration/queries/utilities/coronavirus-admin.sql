1d5b34c76b9235e98ce841371eb9ea11a90f1dec
Calum MacUisdean <calum.macuisdean@gmail.com>
2020-04-01 08:14:35 +0100

    date filter on council search

--- configuration/queries/utilities/coronavirus-admin.sql
+++
@@ -1,1 +0,0 @@
-SELECT DISTINCT council, COUNT(email) AS submissions FROM newsletter_coronavirus WHERE consent_volunteering_updates = true AND consent_tsi = true AND location_postcode != '' AND council != '' GROUP BY council ORDER BY council ASC;
