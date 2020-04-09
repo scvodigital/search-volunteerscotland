5835ab9324237b399f6e66478dc83c9906b8aa17
Calum MacUisdean <calum.macuisdean@gmail.com>
2020-04-09 11:57:04 +0100

    added date and postcode to data export

--- configuration/queries/utilities/coronavirus-admin-results.sql
+++ configuration/queries/utilities/coronavirus-admin-results.sql
@@ -1,5 +1,5 @@
 {{#if @root.request.params.query.council ~}}
-SELECT first_name,last_name,email,constituency,nuts_3,simd_datazone,simd_rank,ward,tsi FROM newsletter_coronavirus WHERE consent_volunteering_updates = true AND consent_tsi = true AND council = {{{safeStringify @root.request.params.query.council}}} AND cast(date_last_modified as date) between '{{#unless @root.request.params.query.from_date}}2020-03-29{{else}}{{@root.request.params.query.from_date}}{{/unless}}' and CURDATE() ORDER BY date_last_modified ASC;
+SELECT date_last_modified,first_name,last_name,email,location_postcode,constituency,nuts_3,simd_datazone,simd_rank,ward,tsi FROM newsletter_coronavirus WHERE consent_volunteering_updates = true AND consent_tsi = true AND council = {{{safeStringify @root.request.params.query.council}}} AND cast(date_last_modified as date) between '{{#unless @root.request.params.query.from_date}}2020-03-29{{else}}{{@root.request.params.query.from_date}}{{/unless}}' and CURDATE() ORDER BY date_last_modified ASC;
 {{else}}
 SELECT email FROM newsletter_coronavirus WHERE 1 = 0
 {{/if ~}}
